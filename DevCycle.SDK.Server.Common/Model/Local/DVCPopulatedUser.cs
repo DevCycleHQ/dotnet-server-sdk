3d387c33e2dea887f73c23b2e6df32608e022ca7
Jamie Sinn <jamie.sinn@taplytics.com>
2022-05-24 12:27:45 -0400

    Remove unused isanon prop.

--- DevCycle.SDK.Server.Common/Model/Local/DVCPopulatedUser.cs
+++ DevCycle.SDK.Server.Common/Model/Local/DVCPopulatedUser.cs
@@ -12,9 +12,7 @@ namespace DevCycle.SDK.Server.Common.Model.Local
         
         private static readonly string DefaultSdkVersion = typeof(DVCPopulatedUser).Assembly.GetName().Version.ToString();
         private static readonly string DefaultPlatformVersion = System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription;
-        
-        private bool isAnonymous;
-        
+
         [DataMember(Name="user_id", EmitDefaultValue=false)]
         [JsonProperty("user_id")]
         public string UserId { get; }
