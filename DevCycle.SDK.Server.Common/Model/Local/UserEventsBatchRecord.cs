f51b59cf15dd6773c369cc7da13e779d2f9b9c2a
Andrew Traviss <traviss@taplytics.com>
2022-10-04 15:45:06 -0400

    Remove unused constructor

--- DevCycle.SDK.Server.Common/Model/Local/UserEventsBatchRecord.cs
+++ DevCycle.SDK.Server.Common/Model/Local/UserEventsBatchRecord.cs
@@ -7,20 +7,12 @@ namespace DevCycle.SDK.Server.Common.Model.Local
     [DataContract]
     public class UserEventsBatchRecord
     {
-        [DataMember(Name="user", EmitDefaultValue=false, IsRequired = false)]
+        [DataMember(Name="user", EmitDefaultValue=false)]
         [JsonProperty("user")]
-
         public DVCPopulatedUser User { get; }
         
         [DataMember(Name="events", EmitDefaultValue=false)]
         [JsonProperty("events")]
-
         public List<DVCRequestEvent> Events { get; private set; }
-
-        public UserEventsBatchRecord(DVCPopulatedUser user)
-        {
-            User = user;
-            Events = new List<DVCRequestEvent>();
-        }
     }
 }
