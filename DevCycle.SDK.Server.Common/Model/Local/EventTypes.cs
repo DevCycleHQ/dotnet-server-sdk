a5758d631ad1d4fc961a344276517a2363966212
Andrew Traviss <traviss@taplytics.com>
2022-10-04 15:42:37 -0400

    Update to aggregated event types

--- DevCycle.SDK.Server.Common/Model/Local/EventTypes.cs
+++ DevCycle.SDK.Server.Common/Model/Local/EventTypes.cs
@@ -2,7 +2,9 @@ namespace DevCycle.SDK.Server.Common.Model.Local
 {
     public static class EventTypes
     {
+        public static readonly string aggVariableDefaulted = "aggVariableDefaulted";
         public static readonly string variableDefaulted = "variableDefaulted";
+        public static readonly string aggVariableEvaluated = "aggVariableEvaluated";
         public static readonly string variableEvaluated = "variableEvaluated";
     }
 }
