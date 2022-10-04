69fe81b6965e1a573ae17c705cf8f130370326bb
Andrew Traviss <traviss@taplytics.com>
2022-10-04 15:44:31 -0400

    Add missing serialization attributes and setters where needed

--- DevCycle.SDK.Server.Common/Model/Local/DVCRequestEvent.cs
+++ DevCycle.SDK.Server.Common/Model/Local/DVCRequestEvent.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
+using Newtonsoft.Json;
 
 namespace DevCycle.SDK.Server.Common.Model.Local
 {
@@ -10,33 +11,40 @@ namespace DevCycle.SDK.Server.Common.Model.Local
         private readonly List<string> eventTypes = new List<string> {EventTypes.aggVariableEvaluated, EventTypes.aggVariableDefaulted};
         
         [DataMember(Name="type", EmitDefaultValue=false)]
-        public string Type { get; }
+        [JsonProperty("type")]
+        public string Type { get; set; }
         
         [DataMember(Name="target", EmitDefaultValue=false)]
-        public string Target { get; }
+        [JsonProperty("target")]
+        public string Target { get; set; }
         
         [DataMember(Name="customType", EmitDefaultValue=false)]
-        public string CustomType { get; }
+        [JsonProperty("customType")]
+        public string CustomType { get; set; }
         
         [DataMember(Name="user_id", EmitDefaultValue=false)]
-        public string UserId { get; }
+        [JsonProperty("user_id")]
+        public string UserId { get; set; }
         
         [DataMember(Name="date", EmitDefaultValue=false)]
-        public DateTime Date { get; }
+        [JsonProperty("date")]
+        public DateTime Date { get; set; }
         
         [DataMember(Name="clientDate", EmitDefaultValue=false)]
-        public DateTime ClientDate { get; }
+        [JsonProperty("clientDate")]
+        public DateTime ClientDate { get; set; }
         
         [DataMember(Name="value", EmitDefaultValue=false)]
+        [JsonProperty("value")]
         public double Value { get; set; }
         
         [DataMember(Name="featureVars", EmitDefaultValue=false)]
-        public Dictionary<string, string> FeatureVars { get; }
+        [JsonProperty("featureVars")]
+        public Dictionary<string, string> FeatureVars { get; set; }
         
         [DataMember(Name="metaData", EmitDefaultValue=false)]
-        public Dictionary<string, object> MetaData { get; }
-        
-        public bool IsCustomEvent { get; }
+        [JsonProperty("metaData")]
+        public Dictionary<string, object> MetaData { get; set; }
 
         public DVCRequestEvent()
         {
@@ -54,14 +62,8 @@ namespace DevCycle.SDK.Server.Common.Model.Local
                 throw new ArgumentException("UserId cannot be null");
             }
 
-            if (!eventTypes.Contains(@event.Type))
-            {
-                IsCustomEvent = true;
-            }
-
-            Type = IsCustomEvent ? "customEvent" : @event.Type;
+            Type = !eventTypes.Contains(Type) ? "customEvent" : @event.Type;
             Target = @event.Target;
-            CustomType = IsCustomEvent ? @event.Type : null;
             UserId = userId;
             Date = DateTimeOffset.UtcNow.DateTime;
             ClientDate = @event.Date ?? DateTimeOffset.UtcNow.DateTime;
