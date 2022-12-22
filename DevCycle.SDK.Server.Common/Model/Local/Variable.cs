dc9a9c8ae650992478a4de1be0c8e3ab668d2318
Adam Wootton <adam@taplytics.com>
2022-12-22 16:36:27 -0500

    include isDefaulted in local variable serialization (#45)

--- DevCycle.SDK.Server.Common/Model/Local/Variable.cs
+++ DevCycle.SDK.Server.Common/Model/Local/Variable.cs
@@ -111,6 +111,8 @@ namespace DevCycle.SDK.Server.Common.Model.Local
         /// <value>Unique key by Project, can be used in the SDK / API to reference by &#x27;key&#x27; rather than _id.</value>
         [DataMember(Name = "key")]
         public string Key { get; set; }
+        
+        [DataMember(Name = "isDefaulted")]
         public bool IsDefaulted { get; set; }
         public string EvalReason { get; set; }
 
